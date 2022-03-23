4af8d735ecea94b434eeab7f66e42017f4692dfa
lanplustechhy <zh841647914@163.com>
2022-03-23 04:17:22 -0700

    3月23日debug版本

---
+++ src/rospolicy/src/Message.cpp
@@ -0,0 +1,32 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-21 02:57:48
+ * @LastEditTime: 2022-03-23 03:36:44
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /catkin_ws/src/rospolicy/src/Message.cpp
+ */
+
+
+#include "Message.h"
+
+
+    
+// std::string TcpMessage::TcpControl(){
+//     _TcpControlMsg["type"] = "response";
+//     _TcpControlMsg["command"] = "/api/joy_control";
+//     _TcpControlMsg["uuid"] = "";
+//     _TcpControlMsg["status"] = "OK";
+//     _TcpControlMsg["error_msg"] = "";
+    
+//     std::string data = _writer.write(_TcpControlMsg);
+//     return data;
+// }
+
+// std::string TcpMessage::TcpDock(){
+    
+// }
+
+// std::string TcpMessage::TcpNavi(){
+    
+// }
