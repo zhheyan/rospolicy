9943dadf77822feac9fe95f3d130df68bd80917f
lanplustechhy <zh841647914@163.com>
2022-03-29 00:18:19 -0700

    3.29

---
+++ src/rospolicy/include/RoboSqlite.h
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-28 22:45:58
+ * @LastEditTime: 2022-03-29 00:17:40
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /catkin_ws/src/rospolicy/include/RoboSqlite.cpp
+ */
+
+#include <sqlite3.h>
+
+bool GetTargetParam(){   //入参为点位名称
+    
+}
+
+bool InsertTargetParam(){   //入参为点为名称和
+
+}
